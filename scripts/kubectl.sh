dec7343ff7cfb8c34db123dafe4d388874739319
rafaelvieira96 <rafaelvieira96@hotmail.com>
2025-10-28 16:45:41 -0300

    Update kubectl.sh

--- scripts/kubectl.sh
+++ scripts/kubectl.sh
@@ -28,3 +28,9 @@ echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
 
 echo "Configuração do kubectl e k9s concluída!"
+
+echo ">>>>>>>>>>>>>>>>>>>>>>> Instalando Helm"
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
